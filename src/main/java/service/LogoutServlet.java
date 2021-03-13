fb0f106dd6c7041cc01f5bfb10d127e266e37de2
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-03-12 22:15:26 -0800

    Add logout

---
+++ src/main/java/service/LogoutServlet.java
@@ -0,0 +1,35 @@
+package service;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+            writer.println("You are successfully logged out");
+            writer.println("<br><a href='index.jsp'>Login</a>");
+        }
+        else {
+            writer.println("Please login first.");
+            request.getRequestDispatcher("index.jsp").include(request, response);
+        }
+
+        writer.close();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
