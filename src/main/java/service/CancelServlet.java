554f97366ecf5574a7c3cbd9b233c40f54fc86ae
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-03-14 07:47:05 -0700

    write back_end of cancel reservation

---
+++ src/main/java/service/CancelServlet.java
@@ -0,0 +1,37 @@
+package service;
+
+import data.RoomReservationDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CancelServlet")
+public class CancelServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        HttpSession session = request.getSession(false);
+        if (session!=null) {
+            writer.println("<html><body style=\"background-color:powderblue;\">");
+            writer.println("welcome " + session.getAttribute("name"));
+            int reserveCode = Integer.parseInt(request.getParameter("reserveCode"));
+            RoomReservationDao.delete(reserveCode);
+            writer.println("Reservation successfully canceled");
+            writer.println("<br><a href='logout'>Logout</a>");
+            writer.println("</body></html>");
+        }else{
+            writer.println("Please login first");
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
